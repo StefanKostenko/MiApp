08adbbd2fdf86d1ca3810184f10a0479073c77ae
StefanKostenko <kostenkostephan@gmail.com>
2022-11-09 10:01:34 +0100

    pageControler

--- symfony-MiApp/src/Controller/PageController.php
+++ symfony-MiApp/src/Controller/PageController.php
@@ -19,4 +19,10 @@ class PageController extends AbstractController
     {
         return $this->render('page/company.html.twig', []);
     }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', []);
+    }
 }
