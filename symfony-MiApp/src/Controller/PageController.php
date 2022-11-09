32ca24bdcfab3980f8043e122f9bdbc46c525718
StefanKostenko <kostenkostephan@gmail.com>
2022-11-09 09:40:03 +0100

    Plantilla copiada

---
+++ symfony-MiApp/src/Controller/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', []);
+    }
+
+    #[Route('/company', name: 'company')]
+    public function company(): Response
+    {
+        return $this->render('page/company.html.twig', []);
+    }
+}
